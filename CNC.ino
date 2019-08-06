4649ad987e9f44dfe65747917c62eb83ced29e03
Mohammad Ashour <eng.mohammed071@gmail.com>
2019-08-06 12:19:27 +0200

    update pins to newest modification

--- CNC.ino
+++ CNC.ino
@@ -1,15 +1,17 @@
 #include <EEPROM.h>
 int MX0=11,MX1=3,MX2=4,MX3=5;             //X motor
 int MY0=7,MY1=8,MY2=9,MY3=10;             //Y motor
-int RE=12;                               //Pedal relay
-//int start=3;                            //start button
-int reset=2;                            //pause and reset interrupts
-//int limitUp=A4,limitLeft=A5;
+int RE=12;                                //Pedal relay
+
+int reset=2;                             //pause and reset interrupts
+int s1=A0,s2=A1,s3=A2;
+int start=A3;                             //start button
+int limitUp=A4,limitLeft=A5;
 
 
-//int s1=6,s2=5,s3=4;
 
-int running;                          //to check if maching in runnning mode
+
+int running;                            //to check if maching in runnning mode
 int homming;                            //to check if maching is going to home 0,0
 int stopUp,stopLeft;                    //to check if border reached
 int plateNum;                           //contains plate number .. the number shown on 7Seg
@@ -19,15 +21,15 @@ int numX,numY,disX,disY;                //variables determind by the plate numbe
 int Ystatus=0;                          //Motors status
 int Xstatus=0;
 
+
+
 int Mdelay=2;                           //delay for motors         
 int Ddelay=1000;                        //delay for despenser
 
 void setup() {
-  //pinMode(s1,INPUT);
-  //pinMode(s2,INPUT);
-  //pinMode(s3,INPUT);
-  
-//  pinMode(RE,OUTPUT);
+  attachInterrupt(0,resetM,RISING);
+
+  pinMode(RE,OUTPUT);
   pinMode(MX0,OUTPUT);
   pinMode(MX1,OUTPUT);
   pinMode(MX2,OUTPUT);
@@ -37,78 +39,89 @@ void setup() {
   pinMode(MY2,OUTPUT);
   pinMode(MY3,OUTPUT);
   
- // pinMode(limitUp,INPUT);
-  //pinMode(limitLeft,INPUT);
-  //pinMode(start,INPUT);
+  pinMode(RE,OUTPUT);
   
-  //attachInterrupt(0,resetM,RISING);
+  pinMode(limitUp,INPUT);                     //set inputs and pullup resistors
+  pinMode(limitLeft,INPUT);
+  pinMode(start,INPUT);
+  pinMode(s1,INPUT);
+  pinMode(s2,INPUT);
+  pinMode(s3,INPUT);
+  
+  digitalWrite(limitUp,HIGH);
+  digitalWrite(limitLeft,HIGH);
+  digitalWrite(start,HIGH);
+  digitalWrite(s1,HIGH);
+  digitalWrite(s2,HIGH);
+  digitalWrite(s3,HIGH);
+  
+  
+  homming=0;
+  stopUp=1,stopLeft=1;
+  plateNum=0;
+  running=0;
 
   idle();
-
-  homming=0;
-  stopUp=0;
-  stopLeft=0;
-  plateNum=0;
-//  running=0;
-  running=1;
-  Serial.begin(9600);
 }
 
 void loop() {
   plateNum=0;
-  //if(digitalRead(s1))plateNum+=1;
-  //if(digitalRead(s2))plateNum+=2;
-  //if(digitalRead(s3))plateNum+=4;
-  
+  if(!digitalRead(s1))plateNum+=1;
+  if(!digitalRead(s2))plateNum+=2;
+  if(!digitalRead(s3))plateNum+=4;
 
   //set variables of the selected plate
   //numX must be even number or last column will be neglected
   switch(plateNum){
-    case 0: numX=3;numY=3;disX=137;disY=175;break;
-    case 1: numX=4;numY=5;disX=15;disY=20;break;
-    case 2: numX=7;numY=7;disX=2;disY=3;break;
-    case 3: numX=2;numY=1;disX=2;disY=3;break;
-    case 4: numX=2;numY=1;disX=2;disY=3;break;
-    case 5: numX=2;numY=1;disX=2;disY=3;break;
-    case 6: numX=2;numY=1;disX=2;disY=3;break;
-    case 7: numX=2;numY=1;disX=2;disY=3;break;
+    case 0: numX=3;numY=3;disX=175;disY=137;break;
+    case 1: numX=6;numY=7;disX=175;disY=137;break;
+    case 2: numX=2;numY=4;disX=175;disY=137;break;
+    case 3: numX=1;numY=6;disX=175;disY=137;break;
+    case 4: numX=6;numY=5;disX=175;disY=137;break;
+    case 5: numX=6;numY=1;disX=175;disY=137;break;
+    case 6: numX=2;numY=2;disX=175;disY=137;break;
+    case 7: numX=4;numY=3;disX=175;disY=137;break;
   }
 
+  //if homming mode go home
+  if(homming)goHome();  
   
   
   //start the program
-//  if(digitalRead(start)) running=1;
+  if(!digitalRead(start)) running=1;
 
   
-  if(homming)goHome();  
-  
   while(running){
-    for(int i=0;i<numX/2;i++){
+    //go to origin
+    goHome();
+
+    
+    for(int i=0;i<numY/2;i++){
         
-      for(int j=1;j<numY;j++){                      //despense and move down
+      for(int j=1;j<numX;j++){                      //despense and move down
         despenser();                              
-        moveDown(disY);
+        moveRight(disX);
       }
       
       despenser();                                  //despense then move left
-      moveRight(disX);
+      moveUp(disY);
       
-      for(int j=1;j<numY;j++){                      //despense and move up
+      for(int j=1;j<numX;j++){                      //despense and move up
         despenser();
-        moveUp(disY);
+        moveLeft(disX);
       }
       
       despenser();                                  //last terminal in the round
-      if(i!=numX/2-1){                              //prevent moving left in the last round
-        moveRight(disX);
-      }
-        
+      if(i!=numY/2-1){                              //prevent moving left in the last round
+        moveUp(disY);
+      }  
     }
-    if(numX%2!=0){
-      moveRight(disX);
-      for(int j=1;j<numY;j++){                      //despense and move down
+    //special case for odd number
+    if(numY%2!=0){
+      moveUp(disY);
+      for(int j=1;j<numX;j++){                      //despense and move down
         despenser();                              
-        moveDown(disY);
+        moveRight(disX);
       }
       despenser();                              
     }
@@ -120,25 +133,22 @@ void loop() {
 
 void resetM(){
   running=0;                                      //to break moving functions
-  homming=1;                                        //to start going home function  
-  stopUp=0;                                         //limits switches checkers
-  stopLeft=0;
+  homming=1;                                      //to start going home function  
 }
 
 void goHome(){
+  stopUp=0;
+  stopLeft=0;
   while(!stopUp||!stopLeft){                        //if any limit switch not reached, continue
-    //if(digitalRead(limitUp))stopUp=1;               //upper limit reached
-    //if(digitalRead(limitLeft))stopLeft=1;           //letf limit reached
-    char c=Serial.read();
-    if(c=='w') stopUp=1;                //limit switches simu
-    if(c=='a') stopLeft=1;
+    if(digitalRead(limitUp))stopUp=1;               //upper limit reached
+    if(digitalRead(limitLeft))stopLeft=1;           //letf limit reached
     
-    if(!stopUp)S_CCX();                              //if upper limit not reached move up
+    if(!stopUp)S_CX();                              //if upper limit not reached move up
     if(!stopLeft)S_CY();                           //if left limit not reached move left
     delay(Mdelay);
-    
   }
   homming=0;
+  idle();                                           //no brake
 }
 
 void despenser(){
@@ -161,7 +171,7 @@ void idle(){
    digitalWrite(MY2,LOW);
    digitalWrite(MY3,LOW);
    
-//   digitalWrite(RE,LOW);
+   digitalWrite(RE,LOW);
 }
 
 void S_CCX(){                                  //step clock wise motor Y
@@ -296,7 +306,7 @@ void S_CY(){                                         //step counter clock wise m
       }
 }
 
-void moveUp(int steps){
+void moveLeft(int steps){
   for(int i=0;i<steps;i++){
     if(!running){EEPROM.write(0,1);break;}            //for test // doesnot work without it for no reason
     S_CY();
@@ -304,7 +314,7 @@ void moveUp(int steps){
  }
 }
 
-void moveDown(int steps){
+void moveRight(int steps){
   for(int i=0;i<steps;i++){
     if(!running)break;
     S_CCY();
@@ -314,7 +324,7 @@ void moveDown(int steps){
 
 
 
-void moveRight(int steps){
+void moveDown(int steps){
   for(int i=0;i<steps;i++){
     if(!running)break;
     S_CX();
@@ -322,7 +332,7 @@ void moveRight(int steps){
   }
 }
 
-void moveLeft(int steps){
+void moveUp(int steps){
   for(int i=0;i<steps;i++){
     if(!running)break;
     S_CCX();
