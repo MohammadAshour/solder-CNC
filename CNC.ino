8a876911e2a5f31df1454058644df0305536883c
Ahmed-Abdelrazeq <54239745+Ahmed-Abdelrazeq@users.noreply.github.com>
2019-08-18 16:20:22 +0200

    Remove unneed line
    No need for setting homming by 1 in resetM function

--- CNC.ino
+++ CNC.ino
@@ -133,7 +133,6 @@ void loop() {
 
 void resetM(){
   running=0;                                      //to break moving functions
-  homming=1;                                      //to start going home function  
 }
 
 void goHome(){
